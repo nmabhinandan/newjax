51a867b8f7511dc465a88725aef3c115aecdf8aa
Abhinandan <nmabhinandan@gmail.com>
2015-06-09 14:14:56 +0530

    configured eslint

--- src/newjax.js
+++ src/newjax.js
@@ -1,23 +1,21 @@
-
-class NewJax {
+export default class NewJax {
   constructor(elm) {
     this.elm = elm;
     this.anchors = elm.querySelectorAll('a');
     if (!!(window.history && window.history.pushState)) {
       this.registerEvents();
     }
-
   }
 
   registerEvents() {
-    for (var i = 0; i < this.anchors.length; i++) {
+    for (let i = 0; i < this.anchors.length; i++) {
       let el = this.anchors.item(i);
       el.addEventListener('click', (e) => {
         if (e.button === 0) {
           return;
         }
         if (event.preventDefault) {
-          event.preventDefault()
+          event.preventDefault();
         } else {
           event.returnValue = false;
         }
@@ -29,16 +27,16 @@ class NewJax {
 
   handle(element) {
     let elm = this.elm;
-    document.pushState
     fetch(element.href, {
       method: 'get',
       headers: {
         'X-PJAX': true
       }
-    }).then(function(response) {
-      return response.text();
-    }).then(function(body) {
+    }).then((response) => {
+      response.text();
+    }).then((body) => {
       elm.innerHTML = body;
+      return;
     });
     let url = element.href.replace(/^.*\/\/[^\/]+/, '');
 
